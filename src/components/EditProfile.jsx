ac1c64c927107180a6d2531d523c8b8f1e36d496
Keerthana077 <keerthanasandeep123@gmail.com>
2024-06-21 12:28:50 +0530

    changes done on update profile functionality

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -5,7 +5,12 @@ import axios from 'axios'
 
 const EditProfile = () => {
     const navigate = useNavigate()
-    const [Data,setData] = useState("")
+    const [Data,setData] = useState({
+      username:"",
+      email:"",
+      bio:"",
+      location:""
+    })
     const config = {
         headers: {
             'Content-Type' : 'application/json'
@@ -17,13 +22,23 @@ const EditProfile = () => {
         .then(res => setData(res.data.user))
         .catch(err => console.error(err));
      },[])
-      // console.log(Data)
-      const {username,email,bio,location} = Data
-      const [newuname,setNewuname] = useState("")
-      const [newemail,setNewemail] = useState("")
-      const [newbio,setNewbio] = useState("")
-      const [newlocation,setNewlocation] = useState("")
       
+      const handleChange = (e)=>{
+        const {name,value} = e.target
+        setData({...Data,[name] : value})
+      }
+      const handleUpdate = ()=>{
+        const updatedData = {
+          ...Data
+        }
+        // console.log(updatedData)
+        axios.put('http://localhost:7000/api/users/profile',updatedData,{headers: {
+          'Content-Type' : 'application/json'
+      },
+      withCredentials : true} )
+      // .then(res => console.log("profile updated successfully!!!"))
+      navigate('/home')
+      }
   return (
     <>
     <NavBar/>
@@ -32,22 +47,22 @@ const EditProfile = () => {
     <div className="d-flex landing flex-column flex-wrap  align-items-center justify-content-center text-light bg-success p-3">
     <div className="input-group mb-2">
     <span className="input-group-text text-success" id="username">Username</span>
-    <input type="text" className="form-control" value={username} onChange={(e)=>setNewuname(e.target.value)} aria-label="Username" aria-describedby="username" />
+    <input type="text" className="form-control" name="username" value={Data.username} onChange={(e)=>handleChange(e)} aria-label="Username" aria-describedby="username" />
     </div>
     <div className="input-group mb-2">
     <span className="input-group-text text-success" id="email">Email</span>
-    <input type="email" className="form-control" value={email} onChange={(e)=>setNewemail(e.target.value)} aria-label="Email" aria-describedby="email" />
+    <input type="email" className="form-control" name="email" value={Data.email} onChange={(e)=>handleChange(e)} aria-label="Email" aria-describedby="email" />
     </div>
     <div className="input-group mb-2">
     <span className="input-group-text text-success">Bio</span>
-    <textarea className="form-control" value={bio} onChange={(e)=>setNewbio(e.target.value)} aria-label="Bio"></textarea>
+    <textarea className="form-control" name='bio' value={Data.bio} onChange={(e)=>handleChange(e)} aria-label="Bio"></textarea>
     </div>
     <div className="input-group mb-2">
     <span className="input-group-text text-success" id="location">Location</span>
-    <input type="text" className="form-control" value={location} onChange={(e)=>setNewlocation(e.target.value)} aria-label="Location" aria-describedby="location" />
+    <input type="text" className="form-control" name="location" value={Data.location} onChange={(e)=>handleChange(e)} aria-label="Location" aria-describedby="location" />
     </div>
     <div className="d-flex flex-row">
-    <button className='btn text-success btn-outline-success bg-light m-1' onClick={()=>console.log("updated..")}>Update</button>
+    <button className='btn text-success btn-outline-success bg-light m-1' onClick={()=>handleUpdate()}>Update</button>
     <button className='btn text-success btn-outline-success bg-light m-1' onClick={()=>navigate(-1)}>Back</button>
     </div>                   
     </div>
