f350d8deec690fce8b60c5a99d0db747d9eb4417
Keerthana077 <keerthanasandeep123@gmail.com>
2024-07-01 13:23:21 +0530

    c

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -12,7 +12,7 @@ const HomePage = () => {
     headers: {
         'Content-Type' : 'application/json'
      },
-    //  withCredentials : true
+     withCredentials : false
   };
  useEffect(()=>{
     axios.get(`${url.baseUrl}/allRecipes`,config )
